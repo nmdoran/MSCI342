67103e8dcdb3202ee80c373f8363e116d2542d64
Alikpld <abouladi@uwaterloo.ca>
2020-10-23 14:52:46 -0400

    Sprint 1 1.0 Search
    Just a rough test on reading user input in javascript. WIll be changed later

--- Search.js
+++ Search.js
@@ -5,3 +5,10 @@ console.log('Hello!');
 const search = 5; 
 
 // takes the variable and searches for it in the database
+
+
+//prompt creates prompting functions. Prompt the user for input
+//calls the prompt built in function
+const prompt = require ('prompt-sync')(); 
+const name  = prompt("What item are you looking for?");
+console.log("Here is " + name + " from the database");
