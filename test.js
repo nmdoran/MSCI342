2ab8520279d3f589305b7023397bf6dc835d43b1
Matthew Schneider <matthewsschneiders@gmail.com>
2020-10-26 02:44:53 -0400

    updated unit tests

--- test.js
+++ test.js
@@ -6,23 +6,54 @@ const test = require('tape');
 const env = Object.assign({}, process.env, {PORT: 5000});
 const child = spawn('node', ['index.js'], {env});
 
-test('responds to requests', (t) => {
-  t.plan(4);
+test('index page responds to requests', (t) => {
 
   // Wait until the server is ready
   child.stdout.on('data', _ => {
-    // Make a request to our app
+    // Make a request to app main page
     (async () => {
-      const response = await got('http://127.0.0.1:5000');
+      const response = await got('http://127.0.0.1:5000/');
       // stop the server
       child.kill();
       // No error
       t.false(response.error);
       // Successful response
       t.equal(response.statusCode, 200);
+
       // Assert content checks
-      t.notEqual(response.body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-      t.notEqual(response.body.indexOf("Getting Started on Heroku with Node.js"), -1);
+      t.notEqual(response.body.indexOf("<title>Main Page</title>"), -1);
+      t.notEqual(response.body.indexOf("Main Page"), -1);
+
+      // Ensure greeting welcome date is correct
+      // **Note that currently this just sees the script for the date and not the date itself, so it is not currently working
+      // **Need to migrate to a testing plugin where we can actually manipulate the page itself
+      var todayDate = Date.now().toLocaleString();
+      t.equal(response.body.indexOf(todayDate), -1);
+
     })();
   });
+
+  t.end()
+});
+
+test('add products page responds to requests', (t) => {
+
+  // Wait until the server is ready
+  child.stdout.on('data', _ => {
+    // Make a request to app main page
+    (async () => {
+      const response = await got('http://127.0.0.1:5000/addProduct');
+
+      t.false(response.error);
+      // Successful response
+      t.equal(response.statusCode, 200);
+
+      // Assert content checks
+      t.notEqual(response.body.indexOf("<title>What's Cookin'?</title>"), -1);
+      t.notEqual(response.body.indexOf("What's Cookin'?"), -1);
+
+    })();
+  });
+
+  t.end();
 });
