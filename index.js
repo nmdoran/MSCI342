a8b1b56c2fa5060669c6b973a56698760c86c8b7
Matthew Schneider <matthewsschneiders@gmail.com>
2020-10-26 01:48:26 -0400

    migrates most changes from sprint 1 onto the main page

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ express()
       res.send("Error " + err);
     }
 })
-  .get('/db', async (req, res) => {
+  .get('/db', jsonParser, async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query(`SELECT * FROM products where prod_name ='${req.query.searchParam}'`);
+      const result = await client.query(`SELECT * FROM products where prod_name='${req.query.searchParam}'`);
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
       client.release();
