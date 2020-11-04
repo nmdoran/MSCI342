9bdd8428a14f023206c5d1c2152d440ef00ac96f
Matthew Schneider <matthewsschneiders@gmail.com>
2020-11-03 19:10:53 -0500

    more logging

--- public/client.js
+++ public/client.js
@@ -6,26 +6,20 @@ function init() {
     console.log("Loaded auth2")
     console.log("Auth2", auth2)
     console.log("GoogleAuth:", GoogleAuth)
+    console.log("About to check if signed in")
+    if (auth2.isSignedIn.get()) {
+      var profile = auth2.currentUser.get().getBasicProfile();
+      console.log('ID: ' + profile.getId());
+      console.log('Full Name: ' + profile.getName());
+      console.log('Given Name: ' + profile.getGivenName());
+      console.log('Family Name: ' + profile.getFamilyName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+    }
   if (GoogleAuth) {
     console.log(GoogleAuth.currentUser.get())
   }
   });
-}
-
-console.log("Auth2", auth2)
-if (auth2.isSignedIn.get()) {
-  var profile = auth2.currentUser.get().getBasicProfile();
-  console.log('ID: ' + profile.getId());
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
-}
-
-console.log("GoogleAuth:", GoogleAuth)
-if (GoogleAuth) {
-  console.log(GoogleAuth.currentUser.get())
 }
 
 
