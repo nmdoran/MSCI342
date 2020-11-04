a5811301e80e8f0578b3dbc2239ea8ef617ceeae
Matthew Schneider <matthewsschneiders@gmail.com>
2020-11-03 19:08:51 -0500

    move API load to html

--- public/client.js
+++ public/client.js
@@ -1,5 +1,5 @@
 console.log('Script file loaded successfully');
-<script src="https://apis.google.com/js/platform.js?onload=init" async defer></script>
+
 function init() {
   gapi.load('auth2', function() {
     /* Ready. Make a call to gapi.auth2.init or some other API */
