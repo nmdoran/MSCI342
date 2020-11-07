da861b3483b154fbbc6c5faf2c128901b42bd824
Matthew Schneider <matthewsschneiders@gmail.com>
2020-11-07 15:36:35 -0500

    get auth instance

--- public/client.js
+++ public/client.js
@@ -4,10 +4,12 @@ function init() {
   gapi.load('auth2', function() {
     /* Ready. Make a call to gapi.auth2.init or some other API */
     gapi.auth2.init({ client_id: "428407383068-nec4si48ja9r4pahl3ohp4rtq3cc8v7m.apps.googleusercontent.com" }).then(function(res) {
-      console.log(res)
-      console.log("GoogleAuth:", GoogleAuth)
-      console.log("About to check if signed in:")
-      if (GoogleAuth.isSignedIn.get()) {
+      console.log(res);
+      var auth2 = gapi.auth2.getAuthInstance();
+      console.log(auth2);
+      console.log("Auth2:", auth2);
+      console.log("About to check if signed in:");
+      if (auth2.isSignedIn.get()) {
         var profile = auth2.currentUser.get().getBasicProfile();
         console.log('ID: ' + profile.getId());
         console.log('Full Name: ' + profile.getName());
@@ -16,8 +18,8 @@ function init() {
         console.log('Image URL: ' + profile.getImageUrl());
         console.log('Email: ' + profile.getEmail());
       }
-      if (GoogleAuth) {
-        console.log(GoogleAuth.currentUser.get())
+      if (auth2) {
+        console.log(auth2.currentUser.get())
       }
     })
     
