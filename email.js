efe11b3aa2172be84a6e47ea593018ff026076c0
nandatanvi <73038903+nandatanvi@users.noreply.github.com>
2020-11-08 15:44:02 -0500

    edited email subject line and message

--- email.js
+++ email.js
@@ -4,10 +4,14 @@ var nodemailer = require('nodemailer');
 var mailOptions = {
     from: 'whatscookinggoodlooking14@gmail.com',
     to: 'palak04021999@gmail.com',
-    subject:'Test',
-    text:`Hope this works LMAO
-            What's Cooking Good Looking? 
-            This is what's expiring soon!`
+    subject:`'What's Cooking Good Looking Expiry Notification`,
+    text:`
+    Hello!
+
+    There are items in your fridge which are going to expire sometime in the next week!
+    Please visit your "What's Cooking Good Looking" account to find out what's expiring soon.
+    
+    Hope you have a great week!`
 };
 
 var transporter = nodemailer.createTransport (
@@ -19,8 +23,8 @@ var transporter = nodemailer.createTransport (
         }
     }
 );
-// email trigger
-cron.schedule('01 00 09 * * Sunday', () => {
+// email trigger 
+cron.schedule('01 35 15 * * Sunday', () => {
     console.log('node email.js');
     transporter.sendMail(mailOptions, function(error,info){
         if(error) {
